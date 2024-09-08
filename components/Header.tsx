676413d888aa362b4cd80a4bb9ea8fbf8177f562
kira07sj <kirubeldagne1@gmail.com>
2024-09-08 16:36:55 -0700

    Menu sidebar added

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,19 +1,34 @@
 import React from 'react'
-import { StyleSheet, View, Text, ImageBackground} from 'react-native'
+import { StyleSheet, View, Text, ImageBackground, TouchableOpacity} from 'react-native'
+import { useState } from 'react';
+import MenuOverlay from './MenuOverlay';
 
 function Header(){
+  
+  
+  const [isOverlay, setIsOverlay] = useState(false);
+
+  function submit(){
+
+  }
+
   return (
     <View style ={styles.container}>
       <ImageBackground
           source={require('../assets/logo.png')}
           style={styles.img}
       ></ImageBackground>
-      <ImageBackground
-      source={require('../assets/more.png')}
-      style={styles.more}
-      >
+      <TouchableOpacity  onPress={()=>setIsOverlay(true)}>
+        <ImageBackground
+        source={require('../assets/more.png')}
+        style={styles.more}/>
+      </TouchableOpacity>
 
-      </ImageBackground>
+      <MenuOverlay
+        visible={isOverlay}
+        onClose={()=> setIsOverlay(false)}
+        
+      />
     </View>
   )
 }
@@ -21,19 +36,22 @@ function Header(){
 const styles = StyleSheet.create({
     container: {
       width: '100%',
-      height:25,
+      height:65,
       flexDirection: 'row',
-      justifyContent: 'space-between',
-      marginTop:50,
-      marginBottom:30,
+      justifyContent: 'space-between',      
+      backgroundColor:'#fff',
+      marginTop:30,
+      alignItems:'center',
+      
     },
     img:{
       width:120,
+      height:25
     },
     more:
   {
     width: 35,
-    marginRight: 5
+    height:35
   }
   });
 
---
+++ components/MenuOverlay.tsx
@@ -0,0 +1,101 @@
+import React, { useState } from 'react';
+import { Modal, View, Text, TextInput, StyleSheet, TouchableOpacity, ImageBackground } from 'react-native';
+import RNPickerSelect from 'react-native-picker-select';
+import { Balance } from '../types';
+
+interface MenuOverlayProps {
+  visible: boolean;
+  onClose: () => void;
+  
+
+}
+
+const MenuOverlay: React.FC<MenuOverlayProps> = ({ visible, onClose }) => {
+ 
+
+  return (
+    <Modal visible={visible} animationType="fade" transparent onRequestClose={onClose}>
+      <TouchableOpacity style={styles.overlayContainer} >
+        <View style={styles.overlayContent}>
+          <TouchableOpacity onPress={onClose}>
+            <ImageBackground style={styles.closeBtn} source={require('../assets/back.png')} />
+          </TouchableOpacity>
+          
+          
+          
+        </View>
+      </TouchableOpacity>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  overlayContainer: {
+    
+  },
+  overlayContent: {
+    width: '70%',
+    height: 600,
+    padding: 20,
+    backgroundColor: 'white',
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.5,
+    shadowRadius: 4,
+    elevation:3,
+    display: 'flex',
+    alignItems: 'center',
+    position:'absolute',
+    right:1
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    
+  },
+  input: {
+    width: '95%',
+    height: 45,
+    borderColor: 'gray',
+    borderWidth: 1,
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    borderRadius: 5
+  },
+  btn: {
+    width: 100,
+    height: 35,
+    display: 'flex',
+    backgroundColor: '#69FAD7',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 5,
+    elevation: 3
+  },
+  closeBtn: {
+    width: 35,
+    height: 30,
+    position: 'relative',
+    marginRight: 200,
+    marginBottom: 5,
+    marginTop: -10
+  },
+  btnText: {
+    fontSize: 15,
+    fontWeight: '700'
+  },
+  dropdown: {
+    borderWidth: 1,
+    width: '95%',
+    height: 45,
+    borderColor: 'gray',
+    marginBottom: 15,
+    paddingHorizontal: 10,
+    borderRadius: 5,
+    display: 'flex',
+    justifyContent: 'center'
+  }
+});
+
+export default MenuOverlay;
